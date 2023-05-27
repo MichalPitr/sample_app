2b459b36ef4729fdd692a32ef0b9bda899dba397
Michal Pitr <michal.pitr.altis@gmail.com>
2023-05-28 00:42:46 +0200

    add parts of user following and buttons

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                  foreign_key: "followed_id",
+                                  dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
 
   attr_accessor :remember_token, :activation_token, :reset_token
 
@@ -76,6 +85,19 @@ class User < ApplicationRecord
     # question mark ensures 'id' is escaped before it gets included in the query - sql injection protecton
     Micropost.where("user_id = ?", id)
   end
+
+  def follow(other_user)
+    following << other_user unless self == other_user
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
 
   private
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -81,4 +81,21 @@ class UserTest < ActiveSupport::TestCase
       @user.destroy
     end
   end
+
+  test 'should follow and unfollow users' do
+    michael = users(:michael)
+    archer = users(:archer)
+    assert archer.valid?
+    assert michael.valid?
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    assert archer.followers.include?(michael)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+
+    # User cannot follow themselves!
+    michael.follow(michael)
+    assert_not michael.following?(michael)
+  end
 end
