07d5b85000643109fe97a4eea58f90a8d5bc50ce
Michal Pitr <michal.pitr.altis@gmail.com>
2023-05-28 23:26:03 +0200

    add full user following

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,22 @@
+class RelationshipsController < ApplicationController
+  # by default applies to all actions for a given controller
+  before_action :logged_in_user
+
+  def create
+    @user = User.find(params[:followed_id])
+    current_user.follow(@user)
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.turbo_stream
+    end
+  end
+
+  def destroy
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow(@user)
+    respond_to do |format|
+      format.html { redirect_to @user, status: :see_other }
+      format.turbo_stream
+    end
+  end
+end
--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -15,6 +15,8 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     assert_select 'h1>img.gravatar'
     assert_match @user.microposts.count.to_s, response.body
     assert_select 'div.pagination', count: 1
+    assert_select '#following', test: @user.following.count
+    assert_select '#followers', test: @user.followers.count
 
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
